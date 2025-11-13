063b89c0cc077b7a8eefd077aa37e4ef5b81f645
Shaurya1269 <132533197+Shaurya1269@users.noreply.github.com>
2025-11-13 18:57:18 +0530

    Delete basic.py

--- basic.py
+++
@@ -1,6 +0,0 @@
-n = 5
-#i controls the rows and j controls the number of stars in the row
-for i in range(1, n+1):
-    for j in range(i):
-        print("*",end="")     #print star without new line
-    print()                   #move to next line after each row
